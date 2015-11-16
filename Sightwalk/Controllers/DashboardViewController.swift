9210e2dde5703a533f5cf07b379a372ef94be9db
Intrexion <ndemie@gmail.com>
2015-11-16 16:16:35 +0100

    adds dashboard storyboard with segmented control view changing

---
+++ Sightwalk/Controllers/DashboardViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DashboardViewController.swift
+//  Sightwalk
+//
+//  Created by Nigel de Mie on 16/11/15.
+//  Copyright © 2015 Sightwalk. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var segmentedControlOutlet: UISegmentedControl!
+    
+    @IBOutlet weak var activityViewOutlet: UIView!
+    @IBOutlet weak var statisticViewOutlet: UIView!
+    @IBOutlet weak var favoriteViewOutlet: UIView!
+
+    
+    @IBAction func indexChangedAction(sender: UISegmentedControl) {
+        switch segmentedControlOutlet.selectedSegmentIndex {
+        case 0:
+            activityViewOutlet.hidden = false;
+            statisticViewOutlet.hidden = true;
+            favoriteViewOutlet.hidden = true;
+        case 1:
+            activityViewOutlet.hidden = true;
+            statisticViewOutlet.hidden = false;
+            favoriteViewOutlet.hidden = true;
+        case 2:
+            activityViewOutlet.hidden = true;
+            statisticViewOutlet.hidden = true;
+            favoriteViewOutlet.hidden = false;
+        default:
+            break;
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityViewOutlet.hidden = false;
+        statisticViewOutlet.hidden = true;
+        favoriteViewOutlet.hidden = true;
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
