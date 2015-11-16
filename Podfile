425ad919476fbe0209f55a817e35de9d3e0e1950
Boris Besemer <borisbesemer@gmail.com>
2015-11-16 14:49:45 +0100

    adds login persistence helper

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'Sightwalk' do
 	pod 'Alamofire', '~> 3.0'
 	pod 'SwiftyJSON', :git => 'https://github.com/SwiftyJSON/SwiftyJSON.git'
 	pod 'JLToast', '~> 1.3'
+	pod 'Locksmith', '~> 2.0'
 end
 
 target 'SightwalkTests' do
---
+++ Sightwalk/Helpers/KeychainHelper.swift
@@ -0,0 +1,61 @@
+//
+//  KeychainHelper.swift
+//  Sightwalk
+//
+//  Created by Boris Besemer on 16-11-15.
+//  Copyright © 2015 Sightwalk. All rights reserved.
+//
+
+import Foundation
+import Locksmith
+
+class LoginPersistenceHelper {
+    // Singleton instancing
+    class var SharedInstance: LoginPersistenceHelper {
+        struct Singleton {
+            static let instance = LoginPersistenceHelper()
+        }
+        return Singleton.instance
+    }
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    
+    func saveToken(username: String, token: String, onCompletion: Void -> Void) {
+        userDefaults.setObject(username, forKey: "userNameKey")
+        
+        do {
+            try Locksmith.saveData(["token": token], forUserAccount: username)
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        onCompletion() // Send this to a store
+    }
+    
+    func accessToken(onCompletion: Void -> Void) {
+        guard let name = userDefaults.stringForKey("userNameKey") else {
+            print("Could not get user key")
+            return
+        }
+        
+        let dictionary = Locksmith.loadDataForUserAccount(name)
+        
+        // Do something with the data here
+        print(dictionary)
+    }
+    
+    func updateToken(token: String, onCompletion: Void -> Void) {
+        guard let name = userDefaults.stringForKey("userNameKey") else {
+            print("Could not get user key")
+            return
+        }
+        
+        do {
+            try Locksmith.updateData(["token": token], forUserAccount: name)
+        } catch let error as NSError {
+            print(error)
+        }
+        
+        onCompletion() // Send this to a store
+    }
+}
