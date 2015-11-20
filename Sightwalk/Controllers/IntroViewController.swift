1902695ca4fc28412e8bc1fc1afe0f5002f65849
Intrexion <ndemie@gmail.com>
2015-11-20 15:21:41 +0100

    adds walkthrough tutorial
    does not include content!
    
    Constraints on login/register button page needs fixing (SightWalk Scene)

--- Sightwalk/Controllers/IntroViewController.swift
+++ Sightwalk/Controllers/IntroViewController.swift
@@ -14,7 +14,6 @@ class IntroViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageViewOutlet.image = UIImage(named: "intro_background")
     }
 
     override func didReceiveMemoryWarning() {
