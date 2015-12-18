80bffe5de84121b17bb7a601dcb3e432b147aa9c
Intrexion <ndemie@gmail.com>
2015-12-18 13:36:12 +0100

    adds showing of favourites in dashboard

--- Sightwalk/Controllers/FavoriteViewController.swift
+++ Sightwalk/Controllers/FavoriteViewController.swift
@@ -7,29 +7,58 @@
 //
 
 import UIKit
+import CoreData
+import Alamofire
+import Foundation
 
 class FavoriteViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var tableData = [Sight]();
+    let imageDownloader = ImageDownloadHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.tableData = SightStore.sharedInstance.favorites
+        self.tableView.reloadData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    
+    override func viewWillAppear(animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        self.tableData = SightStore.sharedInstance.favorites
+        self.tableView.reloadData()
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("favoriteCell", forIndexPath: indexPath) as! FavoriteTableViewCell
+        let row = indexPath.row
+        
+        imageDownloader.downloadImage(tableData[row].imgurl, onCompletion: { response in
+            cell.imageView?.image = UIImage(data: response)
+        })
+        
+        cell.sightTitleLabel.text = tableData[row].title
+        cell.sightDescriptionLabel.text = tableData[row].shortdesc
+        
+        return cell
+    }
 }
+
