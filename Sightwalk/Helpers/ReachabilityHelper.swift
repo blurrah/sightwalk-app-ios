0d275cc16159a1a7013be15d4b0997e69c3462fe
Intrexion <ndemie@gmail.com>
2016-01-08 09:56:46 -0800

    reachabilityhelper fix

--- Sightwalk/Helpers/ReachabilityHelper.swift
+++ Sightwalk/Helpers/ReachabilityHelper.swift
@@ -7,32 +7,23 @@
 //
 
 import Foundation
+import SystemConfiguration
 
 public class ReachabilityHelper {
     
-    class func isConnectedToNetwork()->Bool{
-        var Status:Bool = false
-        let url = NSURL(string: "http://google.com/")
-        let request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "HEAD"
-        request.cachePolicy = NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData
-        request.timeoutInterval = 10.0
-        let session = NSURLSession.sharedSession()
-        
-        session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
-            print("data \(data)")
-            print("response \(response)")
-            print("error \(error)")
-            
-            if let httpResponse = response as? NSHTTPURLResponse {
-                print("httpResponse.statusCode \(httpResponse.statusCode)")
-                if httpResponse.statusCode == 200 {
-                    Status = true
-                }
-            }
-            
-        }).resume()
-        
-        return Status
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
 }
